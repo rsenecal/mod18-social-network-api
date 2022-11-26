ca43b38603f7d45ada6aa2acbfe7baab6db21ea5
rsenecal <roodysenecal@gmail.com>
2022-11-25 20:28:09 -0500

    User Contoller

--- controllers/thought.js
+++ controllers/thought.js
@@ -108,6 +108,35 @@ const thought = {
             .catch((err) => {
                 res.json(err);
             });
-    }
+    },
 
-}
+    // add reaction
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $addToSet: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought with this id" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // delete reaction
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.json(err));
+  },
+};
+
+module.exports = thought;
